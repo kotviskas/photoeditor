9a24147b77293eb84e9000b056dbba57d5920480
kotviskas <47895011+kotviskas@users.noreply.github.com>
2020-04-29 13:51:30 -0500

    изменение размеров и привязок

--- app/src/main/java/com/serezha2001/photoeditor/ui/one/HomeViewModel.java
+++ app/src/main/java/com/serezha2001/photoeditor/ui/one/HomeViewModel.java
@@ -1,16 +1,16 @@
-package com.serezha2001.photoeditor.ui.gallery;
+package com.serezha2001.photoeditor.ui.one;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
