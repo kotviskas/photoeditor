5a8b13941a56b1e38a339977333f726d04bf6a0e
kotviskas <47895011+kotviskas@users.noreply.github.com>
2020-05-05 15:40:02 -0500

    555

--- app/src/main/java/com/serezha2001/photoeditor/ui/seven/SevenFragment.java
+++ app/src/main/java/com/serezha2001/photoeditor/ui/seven/SevenFragment.java
@@ -1,4 +1,4 @@
-package com.serezha2001.photoeditor.ui.six;
+package com.serezha2001.photoeditor.ui.seven;
 
 import android.os.Bundle;
 
@@ -13,13 +13,13 @@ import com.serezha2001.photoeditor.MainActivity;
 import com.serezha2001.photoeditor.R;
 
 
-public class SixFragment extends Fragment {
+public class SevenFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_six, container, false);
+        return inflater.inflate(R.layout.fragment_seven, container, false);
     }
 }
