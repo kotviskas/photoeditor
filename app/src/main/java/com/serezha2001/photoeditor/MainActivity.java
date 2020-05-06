a2370dae4028ae9f98721a8bd22b14507214714b
kotviskas <47895011+kotviskas@users.noreply.github.com>
2020-05-06 05:03:03 -0500

    555

--- app/src/main/java/com/serezha2001/photoeditor/MainActivity.java
+++ app/src/main/java/com/serezha2001/photoeditor/MainActivity.java
@@ -115,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.four,R.id.five,R.id.six,R.id.seven)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.four,R.id.five,R.id.six,R.id.seven,R.id.eight,R.id.nine)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
