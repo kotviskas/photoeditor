9e64e901b95465da7bf355ec7635b7f9aff58872
Alexander Dashuk <pinygod@mail.ru>
2020-05-18 20:19:43 +0700

    Центрирование ретуши мяу

--- app/src/main/java/com/serezha2001/photoeditor/ui/RetouchFragment/RetouchFragment.java
+++ app/src/main/java/com/serezha2001/photoeditor/ui/RetouchFragment/RetouchFragment.java
@@ -13,7 +13,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,12 +25,11 @@ import com.serezha2001.photoeditor.R;
 
 
 public class RetouchFragment extends Fragment {
-    public Bitmap prevBitmap, redactBitmap;
-    public TextView brushSizeVal, coefVal;
-    public SeekBar brushSizeSeekbar, coefSeekbar;
-    public int brushSize = 1;
-    public double coef = 0.1;
-    public Button undoBtn;
+    private Bitmap prevBitmap, redactBitmap;
+    private TextView brushSizeVal, coefVal;
+    private int brushSize = 1;
+    private double coef = 0.1;
+    private boolean isCentering = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,11 +39,18 @@ public class RetouchFragment extends Fragment {
         redactBitmap = redactBitmap.copy(redactBitmap.getConfig(), true);
         brushSizeVal = (TextView)root.findViewById(R.id.brushSize);
         coefVal = (TextView)root.findViewById(R.id.coef);
-        brushSizeSeekbar = (SeekBar)root.findViewById(R.id.brushSizeSeekbar);
-        coefSeekbar = (SeekBar)root.findViewById(R.id.coefSeekbar);
-        undoBtn = (Button)root.findViewById(R.id.undoBtn);
+        SeekBar brushSizeSeekbar = (SeekBar) root.findViewById(R.id.brushSizeSeekbar);
+        SeekBar coefSeekbar = (SeekBar) root.findViewById(R.id.coefSeekbar);
+        Button undoBtn = (Button) root.findViewById(R.id.undoBtn);
+        Switch centeringSwitch = (Switch) root.findViewById(R.id.centeringSwitch);
         brushSizeVal.setText("Brush: 1");
         coefVal.setText("Coef: 0.1");
+
+        centeringSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isCentering = isChecked;
+            }
+        });
 
         brushSizeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
@@ -100,11 +108,11 @@ public class RetouchFragment extends Fragment {
                 float scaleFactorX = (arr[2] - arr[0]) / (prevBitmap.getWidth() / arr[6]), scaleFactorY = (arr[3] - arr[1]) / (prevBitmap.getHeight() / arr[6]);
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        retouch((int)((x-arr[0])/scaleFactorX), (int)((y-arr[1])/scaleFactorY), arr[6]);
-                        Toast.makeText(getContext(), (int)((x-arr[0])/scaleFactorX)+" "+(int)((y-arr[1])/scaleFactorY)+" "+arr[6], Toast.LENGTH_LONG).show();
+                        retouch((int)((x-arr[0])/scaleFactorX), (int)((y-arr[1])/scaleFactorY), arr[6], coef);
+                        //Toast.makeText(getContext(), (int)((x-arr[0])/scaleFactorX)+" "+(int)((y-arr[1])/scaleFactorY)+" "+arr[6], Toast.LENGTH_LONG).show();
                         break;
                     case MotionEvent.ACTION_MOVE:
-                        retouch((int)((x-arr[0])/scaleFactorX), (int)((y-arr[1])/scaleFactorY), arr[6]);
+                        retouch((int)((x-arr[0])/scaleFactorX), (int)((y-arr[1])/scaleFactorY), arr[6], coef);
                         break;
                 }
                 return true;
@@ -120,7 +128,7 @@ public class RetouchFragment extends Fragment {
         MainActivity.mainImage.setOnTouchListener(null);
     }
 
-    private void retouch(int x, int y, float scaleRatio) {
+    private void retouch(int x, int y, float scaleRatio, double innerCoef) {
         int averageRed = 0, averageGreen = 0, averageBlue = 0, cnt = 0;
         for (int i = -brushSize; i < brushSize + 1 && ((int)(x*scaleRatio) + i) < redactBitmap.getWidth() && ((int)(x*scaleRatio) + i) >=0; i++){
             for (int j = -brushSize; j < brushSize + 1 && ((int)(y*scaleRatio) + j) < redactBitmap.getHeight() && ((int)(y*scaleRatio) + j) >= 0; j++){
@@ -134,6 +142,10 @@ public class RetouchFragment extends Fragment {
         for (int i = -brushSize; i < brushSize + 1 && ((int)(x*scaleRatio) + i) < redactBitmap.getWidth() && ((int)(x*scaleRatio) + i) >=0; i++){
             for (int j = -brushSize; j < brushSize + 1 && ((int)(y*scaleRatio) + j) < redactBitmap.getHeight() && ((int)(y*scaleRatio) + j) >= 0; j++){
                 int prevPixel = redactBitmap.getPixel(((int)(x*scaleRatio) + i), ((int)(y*scaleRatio) + j));
+                double coef = innerCoef;
+                if (isCentering){
+                   coef = innerCoef + ((Math.abs(2*brushSize) - Math.abs(i) - Math.abs(j)) * 0.01);
+                }
                 redactBitmap.setPixel(((int)(x*scaleRatio) + i), ((int)(y*scaleRatio) + j), Color.rgb((int)(Color.red(prevPixel) + ((averageRed/cnt - Color.red(prevPixel)) * coef)), (int)(Color.green(prevPixel) + ((averageGreen/cnt - Color.green(prevPixel)) * coef)), (int)(Color.blue(prevPixel) + ((averageBlue/cnt - Color.blue(prevPixel)) * coef))));
             }
         }
